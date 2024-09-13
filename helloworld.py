9de16c4bca763716d7ba233803bb41136d122073
Nguyen Thang <thangb2112008@student.ctu.edu.vn>
2024-09-13 05:36:20 -0400

    Modify helloworld.py file

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 print("Hello World")
-<<<<<<< HEAD
 print("Hello OSS Development")
-print("Hello CTU")
-=======
-print("Hello Git")
 print("Hello CTU-CICT")
->>>>>>> test_functions
